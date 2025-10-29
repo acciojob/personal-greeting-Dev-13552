3fef8ad3099e6bbe0617c4055cf6aaedc6a441df
Dev-13552 <devrathour110@gmail.com>
2025-10-29 15:07:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,23 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [isEmpty, setIsEmpty] = useState(true)
+  const [text, setText] useState("")
+  function changeHandler(e){
+    const value = e.target.value
+    if(!value){
+      setIsEmpty(false)
+    }else{
+      setIsEmpty(true)
+      text ="Hello" + e.target.value
+    }
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+      <label>Enter your name: </label>
+      <input type = "text" onChange={()=>{changeHandler(e)}}>
+      !isEmpty && <p>{text}</p>
     </div>
   )
 }
